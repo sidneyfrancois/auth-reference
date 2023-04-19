d14269631804ae51b425a2760adfa2179895b811
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-19 20:50:22 -0300

    add simple authorization routes

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,5 +1,7 @@
 using AuthReference.DTO;
 using AuthReference.Models;
+using AuthReference.Service;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SecureIdentity.Password;
 
@@ -9,7 +11,7 @@ namespace AuthReference;
 [Route("auth")]
 public class AuthenticationController : ControllerBase
 {
-   [HttpPost]
+   [HttpPost("register")]
    public IActionResult CreateAccount([FromBody] RegisterDTO model)
    {
       if (!ModelState.IsValid)
@@ -26,4 +28,24 @@ public class AuthenticationController : ControllerBase
 
       return Ok(user);
    }
+
+   [HttpPost("login")]
+   public IActionResult Login([FromServices] TokenService tokenService)
+   {
+      var token = tokenService.GenerateToken(null);
+      return Ok(token);
+   }
+
+   [Authorize(Roles = "user")]
+   [HttpGet("user")]
+   public IActionResult GetUser() => Ok(User.Identity.Name);
+
+   [Authorize(Roles = "admin")]
+   [HttpGet("admin")]
+   public IActionResult GetAdmin() => Ok(User.Identity.Name);
+
+   [Authorize(Roles = "author")]
+   [HttpGet("author")]
+   public IActionResult GetAuthor() => Ok(User.Identity.Name);
+
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using AuthReference.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
 
