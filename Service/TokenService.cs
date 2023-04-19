5774d8a28202a94b8553ed9ba7956fefc5694213
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-19 20:38:37 -0300

    add token service

---
+++ Service/TokenService.cs
@@ -0,0 +1,39 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AuthReference.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthReference.Service;
+
+public class TokenService
+{
+   public ClaimsIdentity mockClaims()
+      {
+         var listClaims = new ClaimsIdentity(new Claim[]
+         {
+            new Claim(ClaimTypes.Name, "Sidney"),
+            new Claim(ClaimTypes.Role, "user"),
+            new Claim(ClaimTypes.Role, "admin"),
+         });
+
+         return listClaims;
+      }
+
+   public string GenerateToken(User user)
+   {
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+      var claims = mockClaims();
+      var tokenDescriptor = new SecurityTokenDescriptor()
+      {
+         Subject = claims,
+         Expires = DateTime.UtcNow.AddHours(3),
+         SigningCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(key),
+            SecurityAlgorithms.HmacSha256Signature)
+      };
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      return tokenHandler.WriteToken(token);
+   }
+}
