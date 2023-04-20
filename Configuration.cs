d3e4b905c62290efbbe6bb8e54d70dd4ba99fec8
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-20 08:50:43 -0300

    add apiName and apiKey

--- Configuration.cs
+++ Configuration.cs
@@ -3,4 +3,6 @@ namespace AuthReference;
 public static class Configuration
 {
    public static string JwtKey = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+   public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
 }
